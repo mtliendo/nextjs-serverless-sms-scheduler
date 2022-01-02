8cb50307887953b4a8fbf97fed299765ebc35cb7
Michael Liendo <mtliendo@gmail.com>
2022-01-01 23:47:48 -0600

    updated starter files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<AmplifyProvider>
+			<Component {...pageProps} />
+		</AmplifyProvider>
+	)
 }
 
 export default MyApp
